658cc5f0da212c1090ca858fde1387a8e1d255bf
Yakub Yakubov <yakubyakubov2003@mail.ru>
2026-04-26 14:29:58 +0200

    refactor: move frontend to root for Vercel native deployment

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-# Add the project root to the path
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, path)
+# The root directory is now the parent of the /api folder
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, root_dir)
 
 from main import app
 
